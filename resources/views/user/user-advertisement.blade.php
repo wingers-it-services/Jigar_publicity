1e9c3f659c7b010d34caefb1384d7525d4546d97
vishalthakor-wits <vishal.wingersitservices@gmail.com>
2024-07-25 16:20:53 +0530

    search advertisement by name.

--- resources/views/user/user-advertisement.blade.php
+++ resources/views/user/user-advertisement.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <!--**********************************
-                                                                                                                                Content body start
-                                                                                                                    ***********************************-->
+                                                                                                                                                    Content body start
+                                                                                                                                        ***********************************-->
     <div class="content-body">
         <div class="container-fluid">
             <div class="row">
@@ -12,11 +12,8 @@
                     <div class="card">
                         <div class="card-header">
                             <h4 class="card-title">Advertisments</h4>
-
                             <input type="text" id="searchInput" class="form-control" placeholder="Search by industry name"
-                                style="width: 200px;
-                                onkeyup="searchFunction()">
-
+                                onkeyup="searchFunction()" style="width: 250px;">
                         </div>
                         <div class="card-body pb-1">
                             <div id="lightgallery" class="row">
@@ -24,10 +21,10 @@
                                     <a href="{{ asset($advertisment->advertisment_image) }}"
                                         data-exthumbimage="{{ asset($advertisment->advertisment_image) }}"
                                         data-src="{{ asset($advertisment->advertisment_image) }}"
-                                        class="col-lg-3 col-md-6 mb-4">
+                                        class="col-lg-3 col-md-6 mb-4 advertisment-item">
                                         <img src="{{ asset($advertisment->advertisment_image) }}" class="rounded"
                                             alt="" style="width:100%;">
-                                        <div class="text-center mt-2">{{ $advertisment->industry_name }}</div>
+                                        <div class="text-center mt-2 industry-name">{{ $advertisment->industry_name }}</div>
                                     </a>
                                 @endforeach
                             </div>
@@ -37,9 +34,30 @@
                 </div>
             </div>
         </div>
+
     </div>
+
+    <script>
+        function searchFunction() {
+            var input = document.getElementById('searchInput');
+            var filter = input.value.toUpperCase();
+            var items = document.getElementsByClassName('advertisment-item');
+
+            for (var i = 0; i < items.length; i++) {
+                var industryName = items[i].getElementsByClassName('industry-name')[0];
+                if (industryName) {
+                    var txtValue = industryName.textContent || industryName.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        items[i].style.display = "";
+                    } else {
+                        items[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
     <!--**********************************
-                                                                                                                                Content body end
-                                                                                                                    ***********************************-->
+                                                                                                                                                    Content body end
+                                                                                                                                        ***********************************-->
 
 @endsection
