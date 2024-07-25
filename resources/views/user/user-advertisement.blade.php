53e7e5a06427e5459a674a8f9bbc609f69e2ccfd
vishalthakor-wits <vishal.wingersitservices@gmail.com>
2024-07-25 16:14:29 +0530

    added comany name and search box in user advertisement.

--- resources/views/user/user-advertisement.blade.php
+++ resources/views/user/user-advertisement.blade.php
@@ -2,34 +2,44 @@
 @section('title', 'Advertisement')
 @section('content')
 
-<!--**********************************
-                Content body start
-    ***********************************-->
-<div class="content-body">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Advertisments</h4>
-                    </div>
-                    <div class="card-body pb-1">
-                        <div id="lightgallery" class="row">
-                            @foreach($advertisments as $advertisment)
-                            <a href="{{ asset($advertisment->advertisment_image) }}" data-exthumbimage="{{ asset($advertisment->advertisment_image) }}" data-src="{{ asset($advertisment->advertisment_image) }}" class="col-lg-3 col-md-6 mb-4">
-                                <img src="{{ asset($advertisment->advertisment_image) }}" class="rounded" alt="" style="width:100%;">
-                            </a>
-                            @endforeach
+    <!--**********************************
+                                                                                                                                Content body start
+                                                                                                                    ***********************************-->
+    <div class="content-body">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Advertisments</h4>
+
+                            <input type="text" id="searchInput" class="form-control" placeholder="Search by industry name"
+                                style="width: 200px;
+                                onkeyup="searchFunction()">
+
+                        </div>
+                        <div class="card-body pb-1">
+                            <div id="lightgallery" class="row">
+                                @foreach ($advertisments as $advertisment)
+                                    <a href="{{ asset($advertisment->advertisment_image) }}"
+                                        data-exthumbimage="{{ asset($advertisment->advertisment_image) }}"
+                                        data-src="{{ asset($advertisment->advertisment_image) }}"
+                                        class="col-lg-3 col-md-6 mb-4">
+                                        <img src="{{ asset($advertisment->advertisment_image) }}" class="rounded"
+                                            alt="" style="width:100%;">
+                                        <div class="text-center mt-2">{{ $advertisment->industry_name }}</div>
+                                    </a>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
+                    <!-- /# card -->
                 </div>
-                <!-- /# card -->
             </div>
         </div>
     </div>
-</div>
-<!--**********************************
-                Content body end
-    ***********************************-->
+    <!--**********************************
+                                                                                                                                Content body end
+                                                                                                                    ***********************************-->
 
 @endsection
