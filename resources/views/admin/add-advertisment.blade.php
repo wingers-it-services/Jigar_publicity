8d252a6a9be81a00f5e5eba95fbc53e6419146be
Vishal Thakor <vishal.wingersitservices@gmail.com>
2024-07-27 13:01:57 +0530

    fixed advertisement search in admin panel.

--- resources/views/admin/add-advertisment.blade.php
+++ resources/views/admin/add-advertisment.blade.php
@@ -57,8 +57,8 @@
                                                                         </span>
                                                                     </td>
                                                                 </tr>
+                                                                @endforeach
                                                             </tbody>
-                                                            @endforeach
                                                         </table>
                                                     </div>
                                                 </div>
