39bcfc02165eb0fdb32398700c2a236961dd9d6d
Anjali Tiwari <anjalitiwari67269@gmail.com>
2024-07-29 17:43:16 +0530

    export industry and contact

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\IndustryContactExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Http\Request;
+
+class ExportController extends Controller
+{
+    public function export()
+    {
+        return Excel::download(new IndustryContactExport, 'industries_contacts.xlsx');
+    }
+}
