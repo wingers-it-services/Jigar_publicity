7f6acbf90b200844f0eba6b256fb0e1f669bc498
Vishal Thakor <vishal.wingersitservices@gmail.com>
2024-07-31 10:46:43 +0530

    make export-industries route post

--- routes/admin.php
+++ routes/admin.php
@@ -112,10 +112,9 @@ Route::middleware('auth.admin')->group(function () {
 
     Route::get('/payment-details/{id}', [CheckoutController::class, 'getPaymentDetails']);
 
-    Route::post('/admin-payment',[CheckoutController::class,'store'])->name('payment.store');
+    Route::post('/admin-payment', [CheckoutController::class, 'store'])->name('payment.store');
 
-    Route::get('export-industries', [ExportController::class, 'export'])->name('export.industries');
+    Route::post('export-industries', [ExportController::class, 'export'])->name('export.industries');
 
     Route::post('/admin-profile-update', [AdminController::class, 'updateAdminDetails'])->name('updateAdminDetails');
-
 });
