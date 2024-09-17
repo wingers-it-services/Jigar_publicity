a0ee12ac0a6043e634b2dff46fd41816a3b3c753
Vishal Thakor <vishal.wingersitservices@gmail.com>
2024-09-17 11:32:04 +0530

    also logout user when session time is  expired.

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -30,19 +30,33 @@ class UserMiddleware
 
     private function checkAndCreateCookie(Request $request, Closure $next): Response
     {
-        // Check if the 'session_time' cookie exists
-        if (!$request->cookie('session_time')) {
-            $cookie = cookie('session_time', now(), 5);
-            $user = Auth::user();
+        $user = Auth::user();
+
+        // Check if the user is logged in
+        if ($user) {
+            // Check if remaining_time is 0 or less
+            if ($user->remaining_time <= 0) {
+                // Log out the user
+                Auth::logout();
+
+                // Redirect to the home page
+                return redirect('/')->with('status', 'error')->with('message', 'Your Subscription time is expired');
+            }
+
+            // Check if the 'session_time' cookie exists
+            if (!$request->cookie('session_time')) {
+                $cookie = cookie('session_time', now(), 5);
+                // Create the cookie and return the response
+                return $next($request)->withCookie($cookie);
+            }
 
             // Reduce remaining_time by the specified number of seconds
             $user->remaining_time -= 300;
 
             // Save the updated value to the database
             $user->save();
-            return $next($request)->withCookie($cookie);
         }
 
-        return $next($request); // Proceed if the cookie already exists
+        return $next($request); // Proceed if no user is logged in or cookie already exists
     }
 }
