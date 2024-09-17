f5ffd2668a82a1eb132e1c86ae84ceb8c9f1858f
Vishal Thakor <vishal.wingersitservices@gmail.com>
2024-09-17 11:14:34 +0530

    added code in middleware to create cookie and reduce time by 5 min

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -18,9 +18,31 @@ class UserMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::check() && Auth::user()->is_admin == 0) {
-            return $next($request);
+
+            // Call the function to check and create the session_time cookie if needed
+            $response = $this->checkAndCreateCookie($request, $next);
+
+            return $response;
         }
 
-        return redirect('/')->with('status','error')->with('message', 'Not a valid user');
+        return redirect('/')->with('status', 'error')->with('message', 'Not a valid user');
+    }
+
+    private function checkAndCreateCookie(Request $request, Closure $next): Response
+    {
+        // Check if the 'session_time' cookie exists
+        if (!$request->cookie('session_time')) {
+            $cookie = cookie('session_time', now(), 5);
+            $user = Auth::user();
+
+            // Reduce remaining_time by the specified number of seconds
+            $user->remaining_time -= 300;
+
+            // Save the updated value to the database
+            $user->save();
+            return $next($request)->withCookie($cookie);
+        }
+
+        return $next($request); // Proceed if the cookie already exists
     }
 }
